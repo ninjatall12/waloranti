d774f65cc5f819ea391a48883d0b830d63d07db2
ninjatall12 <58861295+ninjatall12@users.noreply.github.com>
2022-12-25 13:14:31 +0000

    Change head offset from static to dynamic

--- waloranti/waloranti/utilities/config.hpp
+++ waloranti/waloranti/utilities/config.hpp
@@ -9,7 +9,7 @@ namespace cfg
 	constexpr int magnet_key{ VK_XBUTTON2 };
 	constexpr int flick_key{ VK_XBUTTON1 };
 	constexpr int head_offset_x{ 2 };
-	constexpr int head_offset_y{ 4 };
+	inline int head_offset_y{ 4 };
 	inline int magnet_fov{ 9 };
 	inline int magnet_smooth{ 8 };
 	inline int magnet_delay_between_shots{ 200 };
